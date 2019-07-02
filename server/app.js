de7f53f512ded88810ad35929d5c6863d86a5af8
Emmanuel Agubata <emmanuel@Ogos-MacBook-Pro.local>
2019-07-02 16:19:47 -0700

    Adds final projects files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 // import files and packages up here
-
-
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data');
+var path = require('path');
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev')); 
 
+app.get('/', (req, res) => {
+    
+res.send ("Hello World");
+});
+
+
+app.get('/data', (req, res) => {
+res.json(data);
+});
 // finally export the express application
 module.exports = app;
+
+
